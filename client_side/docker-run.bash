9f310ee102e913c4750fdb8fe92273f1c5598788
Wilson <wilsonting2025@u.northwestern.edu>
2024-12-01 15:07:53 -0600

    docker file edit for API key access

--- client_side/docker-run.bash
+++ client_side/docker-run.bash
@@ -2,4 +2,4 @@
 #
 # Linux/Mac BASH script to run docker container
 #
-docker run -it -u user -w /home/user -v /Users/Wilson/Desktop/CS310/project02-client-main:/home/user --network="host" --rm project02-client bash
+docker run -it -u user -w /home/user -v /Users/Wilson/Desktop/CS310/project02-client-main:/home/user --network="host" --rm --env-file ../.env project02-client bash
--- server_side/docker-run.bash
+++ server_side/docker-run.bash
@@ -2,4 +2,4 @@
 #
 # Linux/Mac BASH script to run docker container
 #
-docker run -it -u user -w /home/user -v /Users/Wilson/Desktop/CS310/project02-server-main:/home/user -p 8080:8080 --rm project02-server bash
+docker run -it -u user -w /home/user -v /Users/Wilson/Desktop/CS310/project02-server-main:/home/user -p 8080:8080 --rm --env-file ../.env project02-server bash
