2854142ffc07e4b26d632bf615e55d7b07dd7a84
Wilson <wilsonting2025@u.northwestern.edu>
2024-12-07 02:32:21 -0600

    func_3

--- server_side/docker-run.bat
+++ server_side/docker-run.bat
@@ -3,4 +3,4 @@ REM
 REM Windows BATCH script to run docker container
 REM
 @echo on
-docker run -it -u user -w /home/user -v .:/home/user -p 8080:8080 --rm project02-server bash
+docker run -it -u user -w /home/user -v /Users/Wilson/Desktop/CS310/CS310_Final_Project/CS310_Final_Project/server_side:/home/user -p 8080:8080 --rm --env-file ../.env cs310_final_project_server bash
